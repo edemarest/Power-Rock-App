3539c3384e787a3d5a24b9b5f71e1413fb6e081c
edemarest <demarest.e@northeastern.edu>
2024-10-25 13:41:59 -0400

    Initial Commit

---
+++ Power Rock App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Power Rock App
+//
+//  Created by Ella Demarest on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Power Rock AppTests/Power_Rock_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Power_Rock_AppTests.swift
+//  Power Rock AppTests
+//
+//  Created by Ella Demarest on 10/25/24.
+//
+
+import Testing
+@testable import Power_Rock_App
+
+struct Power_Rock_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
